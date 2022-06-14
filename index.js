d812f4cd846791764d302d400991f0e3f6da0403
Sarfaraz Nawaz <sir_nawaz959@yahoo.com>
2022-06-15 02:44:57 +0530

    Invoke videoQueue.getNextJob which causes bclient to be initialized

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const time = () => new Date().getTime() / 1000;
 async function closeBeingSlowIssue() {
   const videoQueue = new Queue('video transcoding', 'redis://127.0.0.1:6379');
 
+  // this causes videoQueue.bclient to be initialized, and videoQueue.bclientInitialized becomes true
+  // then videoQueue.whenCurrentJobsFinished gets executed completely, invoking redisClientDisconnect()
+  // which takes 0.5 second to timeout
+  videoQueue.getNextJob();
+
   const t1 = time();
 
   await videoQueue.close();
